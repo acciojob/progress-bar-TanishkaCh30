6d75ac351f19575dd438ec5612ca7fddc7947872
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-18 12:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+let circles = document.getElementsByClassName("circle");
+let nextBtn = document.querySelector("#next");
+let prevBtn = document.querySelector("#prev");
+let lines = document.getElementsByClassName("prog-line");
+
+prevBtn.disabled=true;
+
+let circleArr = [...circles];
+let linesArr = [...lines];
+
+let count=0;
+
+nextBtn.addEventListener("click" ,()=>{
+	prevBtn.disabled=false;
+	count++;
+	for(let i = 0 ; i<circleArr.length ; i++){
+		circleArr[count].classList.add("active");
+		if(count===circleArr.length-1){
+			nextBtn.disabled=true;
+		}
+	}
+
+	for(let i = 0 ; i<linesArr.length ; i++){
+		linesArr[count-1].classList.add("active");
+	}
+});
+
